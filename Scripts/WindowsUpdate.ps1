a989a5b8c6582534432c6f63efb26b915b04eba8
holbs <aranholbrook@outlook.com>
2024-01-27 19:36:14 +0000

    Updated script to include force and scope params

--- Scripts/WindowsUpdate.ps1
+++ Scripts/WindowsUpdate.ps1
@@ -1,4 +1,4 @@
-Install-PackageProvider -Name NuGet
-Install-Module -Name PSWindowsUpdate -Confirm:$False -Force -Scope Allusers
-Import-Module -Name PSWindowsUpdate
+Install-PackageProvider -Name NuGet -Confirm:$False -Force -Scope AllUsers
+Install-Module -Name PSWindowsUpdate -Confirm:$False -Force -Scope AllUsers
+Import-Module -Name PSWindowsUpdate -Force -Scope Global
 Get-WindowsUpdate -MicrosoftUpdate -UpdateType Software -RootCategories 'Critical Updates','Security Updates' -Install -IgnoreReboot -Confirm:$false
