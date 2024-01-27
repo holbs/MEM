40e931aef447c9f522841bf4b8ec1fa83e8956af
Aran Holbrook <91827424+holbs@users.noreply.github.com>
2024-01-27 15:46:26 +0000

    Updated WinVerifyTrust.ps1
    Added missing ' in comment

--- CI/WinVerifyTrust.ps1
+++ CI/WinVerifyTrust.ps1
@@ -3,7 +3,7 @@ Try {
     # Check both keys exist on the workstation first
     $1 = Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck' -ErrorAction Stop
     $2 = Get-ItemProperty -Path 'HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck' -ErrorAction Stop
-    # Check that the string 'EnableCertPaddingCheck is present and set to 1
+    # Check that the string 'EnableCertPaddingCheck' is present and set to 1
     If ($1.EnableCertPaddingCheck -and $2.EnableCertPaddingCheck) {
         If ($1.EnableCertPaddingCheck -ne 1 -or $2.EnableCertPaddingCheck -ne 1) {
             Return $false
