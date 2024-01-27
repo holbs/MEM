b6e7fdd3c3583e307aeecf2c2178c297c76136b4
holbs <aranholbrook@outlook.com>
2024-01-27 18:57:24 +0000

    Uploaded WindowsUpdate.ps1 which uses PSWindowsUpdate to install updates on stubborn workstations

---
+++ Scripts/WindowsUpdate.ps1
@@ -0,0 +1,4 @@
+Install-PackageProvider -Name NuGet
+Install-Module -Name PSWindowsUpdate -Confirm:$False -Force -Scope Allusers
+Import-Module -Name PSWindowsUpdate
+Get-WindowsUpdate -MicrosoftUpdate -UpdateType Software -RootCategories 'Critical Updates','Security Updates' -Install -Confirm:$false
