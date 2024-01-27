72c820ee9e183d5982995416319ac0d0342c8c10
Aran Holbrook <91827424+holbs@users.noreply.github.com>
2024-01-27 15:35:41 +0000

    Uploaded BirthdayAttack.ps1
    Script remediates CVE-2016-2183 on a Windows workstation

---
+++ CI/BirthdayAttack.ps1
@@ -0,0 +1,15 @@
+#Region: Detection
+Try {
+    $TripleDES168 = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168' -ErrorAction Stop
+    If ($TripleDES168.Enabled -ne 0) {
+        Return $false
+    }
+} Catch {
+    Return $false
+}
+Return $true
+#EndRegion
+#Region: Remediation
+New-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168'
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168' -Name 'Enabled' -Value '0' -Type DWord -Force
+#EndRegion
