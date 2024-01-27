70c161508073ce4a21d148947966ed5f20e8be8a
Aran Holbrook <91827424+holbs@users.noreply.github.com>
2024-01-27 15:38:45 +0000

    Uploaded WinVerifyTrust.ps1
    Script remediates CVE-2013-3900 on a Windows workstation

---
+++ CI/WinVerifyTrust.ps1
@@ -0,0 +1,26 @@
+#Region: Detection
+Try {
+    # Check both keys exist on the workstation first
+    $1 = Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck' -ErrorAction Stop
+    $2 = Get-ItemProperty -Path 'HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck' -ErrorAction Stop
+    # Check that the string 'EnableCertPaddingCheck is present and set to 1
+    If ($1.EnableCertPaddingCheck -and $2.EnableCertPaddingCheck) {
+        If ($1.EnableCertPaddingCheck -ne 1 -or $2.EnableCertPaddingCheck -ne 1) {
+            Return $false
+        }
+    } Else {
+        Return $false
+    }
+} Catch {
+    Return $false
+}
+Return $true
+#EndRegion
+#Region: Remediation
+New-Item -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust'
+New-Item -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust\Config'
+New-ItemProperty -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck' -Value '1' -Type String -Force
+New-Item -Path 'HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust'
+New-Item -Path 'HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config'
+New-ItemProperty -Path 'HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck' -Value '1' -Type String -Force
+#EndRegion
