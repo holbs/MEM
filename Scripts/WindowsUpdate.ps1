87c4d4d613e12de6974803e68ecbaf9a08bc2750
holbs <aranholbrook@outlook.com>
2024-04-01 21:43:37 +0100

    Removed Windows Update script

--- Scripts/WindowsUpdate.ps1
+++
@@ -1,4 +0,0 @@
-Install-PackageProvider -Name NuGet -Confirm:$false -Force -Scope AllUsers
-Install-Module -Name PSWindowsUpdate -Confirm:$false -Force -Scope AllUsers
-Import-Module -Name PSWindowsUpdate -Force -Scope Global
-Get-WindowsUpdate -MicrosoftUpdate -UpdateType Software -RootCategories 'Critical Updates','Security Updates' -Install -IgnoreReboot -Confirm:$false
