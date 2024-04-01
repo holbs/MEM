5c268f061720f2c7ae484de7f59c4378e60a3558
holbs <aranholbrook@outlook.com>
2024-04-01 21:15:47 +0100

    Updated script to use use a button for restarting the workstation

--- WinHealth/CheckWindowsImageHealth.ps1
+++ WinHealth/CheckWindowsImageHealth.ps1
@@ -1,36 +1,51 @@
 #Region: Detection
 If ((Repair-WindowsImage -Online -CheckHealth).ImageHealthState -eq "Healthy") {
     Return $true
-} Else {
-    Return $false
 }
 #EndRegion
-#Region: Remediation
+#Region: Installation
 $CheckHealth = Repair-WindowsImage -Online -CheckHealth
 $ScanHealth = Repair-WindowsImage -Online -ScanHealth
 $RestoreHealth = Repair-WindowsImage -Online -RestoreHealth
-If ($CheckHealth.RestartNeeded -eq $true -or $ScanHealth.RestartNeeded -eq $true -or $RestoreHealth.RestartNeeded -eq $true) {
-    # Prompt the user to restart the workstation by displaying a toast notification
-    Add-Type -AssemblyName System.Windows.Forms
-    Add-Type -AssemblyName System.Drawing
-    $Notification = New-Object System.Windows.Forms.NotifyIcon
-    $Notification.Icon = [System.Drawing.SystemIcons]::Warning
-    $Notification.BalloonTipTitle = "Restart Notification"
-    $Notification.BalloonTipText = "A system scan has found Windows image corruption. Please restart your workstation to complete repairs."
-    $Notification.Visible = $true
-    $Notification.ShowBalloonTip(300000)
-} Else {
+# Create registry settings for a custom URI so PowerShell can exectue scripts from a notification
+New-Item "HKLM:\SOFTWARE\Classes\toastnotification" -Force
+New-Item "HKLM:\SOFTWARE\Classes\toastnotification\DefaultIcon" -Force
+New-Item "HKLM:\SOFTWARE\Classes\toastnotification\shell" -Force
+New-Item "HKLM:\SOFTWARE\Classes\toastnotification\shell\open" -Force
+New-Item "HKLM:\SOFTWARE\Classes\toastnotification\shell\open\command" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\toastnotification" -Name "(default)" -Value "URL:PowerShell Toast Notification Protocol" -PropertyType String -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\toastnotification" -Name "URL Protocol" -Value "" -PropertyType String -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\toastnotification\DefaultIcon" -Name "(default)" -Value "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe,1" -PropertyType String -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\toastnotification\shell" -Name "(default)" -Value "open" -PropertyType String -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\toastnotification\shell\open\command" -Name "(default)" -Value "`"$env:ProgramData\ToastNotification\RestartComputer.cmd`" %1" -PropertyType String -Force
+# Copy restart script to %ProgramData% so it can be called through the notification
+New-Item -Path "$env:ProgramData\ToastNotification" -ItemType Directory -Force
+Copy-Item -Path "$PSScriptHost\RestartComputer.cmd" -Destination "$env:ProgramData\ToastNotification\RestartComputer.cmd" -Force -Confirm:$false # RestartComputer.cmd is in the application content, and triggers the PowerShell script
+Copy-Item -Path "$PSScriptHost\RestartComputer.ps1" -Destination "$env:ProgramData\ToastNotification\RestartComputer.ps1" -Force -Confirm:$false # RestartComputer.ps1 is in the application content, and runs Restart-Computer
+# If the Repair-WindowsImage commands did not require a restart then run sfc /scannow
+If ($CheckHealth.RestartNeeded -ne $true -and $ScanHealth.RestartNeeded -ne $true -and $RestoreHealth.RestartNeeded -ne $true) {
     # Trigger SFC /scannow
     Start-Process -WindowStyle hidden -FilePath "$env:WINDIR\System32\sfc.exe" -ArgumentList "/scannow"
-    Wait-Process -Name "SFC" -ErrorAction SilentlyContinue
-    # Prompt the user to restart the workstation by displaying a toast notification
-    Add-Type -AssemblyName System.Windows.Forms
-    Add-Type -AssemblyName System.Drawing
-    $Notification = New-Object System.Windows.Forms.NotifyIcon
-    $Notification.Icon = [System.Drawing.SystemIcons]::Warning
-    $Notification.BalloonTipTitle = "Restart Notification"
-    $Notification.BalloonTipText = "A system scan has found Windows image corruption. Please restart your workstation to complete repairs."
-    $Notification.Visible = $true
-    $Notification.ShowBalloonTip(300000)
+    Wait-Process -Name "sfc" -ErrorAction SilentlyContinue
 }
+#  Prompt the user to restart via a toast notification
+$ToastXml = @"
+<toast>
+    <visual>
+        <binding template="ToastGeneric">
+            <text>Restart Notification</text>
+            <text>A system scan has found Windows image corruption. Please restart your computer to complete repairs.</text>
+            <image placement="appLogoOverride" src="$PSScriptHost\Restart.png"/>
+        </binding>
+    </visual>
+    <actions>
+        <action content="Snooze" activationType="protocol" arguments="" />
+        <action content="Restart" activationType="protocol" arguments="toastnotification://trigger" />
+    </actions>
+</toast>
+"@
+$XmlDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
+$XmlDocument.LoadXml($ToastXml)
+$AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XmlDocument)
 #EndRegion
