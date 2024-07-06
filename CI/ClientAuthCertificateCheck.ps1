e2c107e7ccac390f3a763883bc8a3c115f102c52
holbs <91827424+holbs@users.noreply.github.com>
2024-07-06 17:21:45 +0100

    Added issuing server to client authentication check script

--- CI/ClientAuthCertificateCheck.ps1
+++ CI/ClientAuthCertificateCheck.ps1
@@ -1,5 +1,6 @@
 #Region: Detection
-$ClientAuthCert = Get-ChildItem -Path "Cert:\LocalMachine\My" | Where-Object {$_.Extensions | Where-Object {$_.Format(0) -match "Client Authenication"}} | Sort-Object NotAfter -Descending | Select-Object -f 1
+$CAIssueServers = @("CN=PKIServer, DC=contoso, DC=com")
+$ClientAuthCert = Get-ChildItem -Path "Cert:\LocalMachine\My" | Where-Object {$_.Issuer -in $CAIssueServers} | Where-Object {$_.Extensions | Where-Object {$_.Format(0) -match "Client Authenication"}} | Sort-Object NotAfter -Descending | Select-Object -f 1
 If ($ClientAuthCert.NotAfter -ge (Get-Date)) {
     Return $true
 } Elseif ($ClientAuthCert.NotAfter -lt (Get-Date)) {
