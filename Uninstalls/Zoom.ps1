cbc9ebe72ec5d90fcce8a70e2be4ff7561b44fdb
holbs <91827424+holbs@users.noreply.github.com>
2026-02-02 15:11:56 +0000

    Detection changed to user installs only. If system installs need removing do detection by registry key: HKLM\SOFTWARE\ZoomUMX

--- Uninstalls/Zoom.ps1
+++ Uninstalls/Zoom.ps1
@@ -1,8 +1,8 @@
 <#
 .SYNOPSIS
-    Uninstalls Zoom.
+    Uninstalls Zoom installed as a user.
 .DESCRIPTION
-    The detection script is used to detect if Zoom is installed by checking both system-wide and per-user installation locations. The uninstall program uses Zoom's CleanZoom.exe utility to silently remove Zoom from the system.
+    The detection script is used to detect if Zoom is installed by checking the per-user installation locations. The uninstall program uses Zoom's CleanZoom.exe utility to silently remove Zoom from the system.
 .NOTES
     Set up an application using the detection script to detect Zoom installations. Set the uninstall script as the uninstall program. Deploy the uninstall program to remove Zoom silently. The uninstall program will also move the cleanzoom.log file to a set folder for troubleshooting.
 .LINK
@@ -10,10 +10,8 @@
 #>
 
 #Region: Detection
-$Installs = @()
-$Installs += Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty | Where-Object {$_.DisplayName -like "Zoom Workplace (*)"}
-$Installs += Get-Item -Path "$env:SystemDrive\Users\*\AppData\Roaming\Zoom\bin\Zoom.exe" -ErrorAction SilentlyContinue
-If ($Installs.Count -gt 0) {
+$Zoom += Get-Item -Path "$env:SystemDrive\Users\*\AppData\Roaming\Zoom\bin\Zoom.exe" -ErrorAction SilentlyContinue
+If ($Zoom) {
     Exit 0
 } Else {
     Exit 1
